70e6c444916e979843a987733943f3e438067593
yigmmk <marvelousme@163.com>
2021-01-28 23:24:07 +0800

    初始化

--- Week09/main.go
+++ Week09/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"context"
-	"fmt"
 	"log"
 	"net"
 	"os"
@@ -47,98 +46,70 @@ func main() {
 				log.Println(rec)
 			}
 		}()
-		conn, err := net.Listen("tcp", "127.0.0.1:9999")
+		listen, err := net.Listen("tcp", "127.0.0.1:9999")
 		if err != nil {
-			log.Println(err)
 			return err
 		}
 
-		return nil
+		for {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+				conn, err := listen.Accept()
+				if err != nil {
+					return errors.WithStack(err)
+				}
+
+				ch := make(chan []byte)
+				defer conn.Close()
+
+				group.Go(func() error {
+					rd := bufio.NewReader(conn)
+					for {
+						select {
+						case <-ctx.Done():
+							return ctx.Err()
+						default:
+							line, _, err := rd.ReadLine()
+							if err != nil {
+								close(ch)
+								return err
+							}
+							ch <- line
+						}
+					}
+					return errors.New("quit read")
+				})
+				group.Go(func() error {
+					wr := bufio.NewWriter(conn)
+					for {
+						select {
+						case <-ctx.Done():
+							return ctx.Err()
+						default:
+							line, ok := <-ch
+							if !ok {
+								return nil
+							}
+
+							if len(line) <= 0 {
+								wr.Write([]byte("-----\n"))
+								continue
+							}
+							wr.WriteString("Hello ")
+							wr.Write(line)
+							wr.WriteString("\n")
+							wr.Flush()
+						}
+					}
+					return errors.New("quit write")
+				})
+			}
+		}
+		return errors.New("accept error ")
 	})
-
-	serve(ctx)
-
 	if err := group.Wait(); err != nil {
 		log.Println(err)
-	}
-}
-
-func serve(ctx context.Context) error {
-	ln, err := net.Listen("tcp", "127.0.0.1:8088")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			conn, err := ln.Accept()
-			if err != nil {
-				fmt.Println(errors.Wrap(err, "accept error"))
-				continue
-			}
-			go handleConn(ctx, conn)
-		}
-	}
-}
-
-func handleConn(ctx context.Context, conn net.Conn) {
-	fmt.Println(conn.RemoteAddr(), "connected")
-
-	defer conn.Close()
-	ch := make(chan []byte)
-	g, _ := errgroup.WithContext(ctx)
-	g.Go(func() error {
-		return read(ctx, conn, ch)
-	})
-
-	g.Go(func() error {
-		return write(ctx, conn, ch)
-	})
-
-	g.Wait()
-	fmt.Println(conn.RemoteAddr(), "closed")
-}
-
-func read(ctx context.Context, conn net.Conn, ch chan []byte) error {
-	rd := bufio.NewReader(conn)
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			line, _, err := rd.ReadLine()
-			if err != nil {
-				close(ch)
-				return err
-			}
-			ch <- line
-		}
-
-	}
-}
-
-func write(ctx context.Context, conn net.Conn, ch chan []byte) error {
-	wr := bufio.NewWriter(conn)
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			line, ok := <-ch
-			if !ok {
-				return nil
-			}
-
-			if len(line) <= 0 {
-				continue
-			}
-			wr.WriteString("Hello ")
-			wr.Write(line)
-			wr.WriteString("\n")
-			wr.Flush()
-		}
 	}
 }
